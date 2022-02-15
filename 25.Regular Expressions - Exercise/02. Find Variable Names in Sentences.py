0ff48e7ac40490ea51ff2b9f8ea429ee59eb980e
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2022-02-15 16:38:10 +0200

    Delete 25.Regular Expressions - Exercise directory

--- 25.Regular Expressions - Exercise/02. Find Variable Names in Sentences.py
+++
@@ -1,7 +0,0 @@
-import re
-text = input()
-pattern = re.compile(r'(^|(?<=\s))_([a-zA-Z0-9]+(?=\s|$))')
-matches = re.finditer(pattern,text)
-
-output = [m.group(2) for m in matches]
-print(','.join(output))
