a452a2c218d2214de855dd9222b034d90dc2f302
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2022-02-15 16:37:34 +0200

    Delete 22.Text Processing - Exercise directory

--- 22.Text Processing - Exercise/04. Caesar Cipher.py
+++
@@ -1,2 +0,0 @@
-intput_text = input()
-print(''.join([chr(ord(x)+3) for x in intput_text]))
--- 22.Text Processing - Exercise/05. Emoticon Finder.py
+++
@@ -1,6 +0,0 @@
-input_string = input()
-
-emoticon_list = [str(input_string[i:i+2]) for i in range(len(input_string)-1) if input_string[i] == ':']
-if emoticon_list:
-    for item in emoticon_list:
-        print(item)
