79fe5afe513f6dee2a87029ad56be32ae921f9d2
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2022-02-26 00:32:21 +0200

    Delete Past Exams directory

--- Past Exams/3_numbers.py
+++
@@ -1,9 +0,0 @@
-read = list(map(int, input().split()))
-
-read = [number for number in read if number > sum(read) / len(read)]
-read = sorted(read, reverse=True)
-
-if len(read) == 0:
-    print("No")
-else:
-    print(" ".join(list(map(str, read[:5]))))
